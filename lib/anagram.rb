15e9889f464a6e4209bc28d3bb47243135a99cef
timtan93 <timtan93@gmail.com>
2018-12-15 13:49:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_list)
+    anagram_list.select {|word| word.split("").sort == self.word.split("").sort }
+  end
+
+
+end
+#x = %w(gallery ballerina regally clergy largely leading)
+#binding.pry
+#p 'eof'
